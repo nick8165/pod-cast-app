8f92d7256c9713798780abc6a3c28c9697c509cd
Nicholas Freeman <nick_freeman65@yahoo.com>
2022-05-24 18:07:26 -0500

    set up for user login persistance

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
-end
+
+    def show
+      user = User.find_by(id: session[:user_id])
+      if user
+        render json: user
+      else
+        render json: { error: "Not authorized" }, status: :unauthorized
+      end
+    end
+
+    def create
+        user = User.create!(user_params)
+        render json: user
+    end
+
+    private
+
+    def render_unprocessable_entity(invalid)
+        render json:{error: invalid.record.errors}, status: :unprocessable_entity
+    end
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
+    end
+
+  end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
     has_many :pod_cast, through: :user_pod_cast
+    has_secure_password
 end
