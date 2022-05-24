c12c02f20254eb780275dbfefd4c5155d78a19ba
Nicholas Freeman <nick_freeman65@yahoo.com>
2022-05-24 18:07:43 -0500

    set routes for login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,16 @@ Rails.application.routes.draw do
   resources :pod_cast_genres
   resources :user_pod_casts
   resources :creators
-  resources :playlists
+  resources :playlists, only: [:create, :index, :show, :update, :destroy]
   resources :genres
   resources :pod_casts
   resources :users
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  
 end
