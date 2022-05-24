f362600c1117ad53b1c73e019323c3701b79805b
Nicholas Freeman <nick_freeman65@yahoo.com>
2022-05-24 18:08:18 -0500

    change password for encrypting

--- db/schema.rb
+++ db/schema.rb
@@ -66,7 +66,7 @@ ActiveRecord::Schema.define(version: 2022_05_24_202934) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
