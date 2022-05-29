9ae4ab9bf0bc0a5f212ed3e94f3ae843b84284f6
Nicholas Freeman <nick_freeman65@yahoo.com>
2022-05-29 15:18:43 -0500

    create podcast component and set up to eventually search for specific podcast

---
+++ client/src/podcastsearch.js
@@ -0,0 +1,34 @@
+import React, {useState} from "react"
+import { FormControl, InputGroup, Button} from "react-bootstrap"
+
+function PodCastSearch() {
+    const [searchValue, setSearchValue] = useState("")
+    function searchChange(e) {
+        setSearchValue(e.target.value)
+    }
+
+    function handleClick() {
+        console.log(searchValue)
+    }
+
+    return(
+        <div className="App" onChange={searchChange}>
+        <div className="container h-100">
+          <div className="row h-100 justify-content-center align-items-center"></div>
+          <InputGroup className="col-6">
+            <FormControl
+                
+                placeholder="Search"
+                aria-label="Search"
+                aria-describedby="basic-addon2"
+            />
+            <Button variant="outline-secondary" id="button-addon2" onClick={handleClick}>
+                Search
+            </Button>
+          </InputGroup>
+        </div>
+      </div>
+    )
+}
+
+export default PodCastSearch
