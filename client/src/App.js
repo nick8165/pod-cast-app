8643197460ad3dd7e1f53cbd6f6fdfbecc9559e6
Nicholas Freeman <nick_freeman65@yahoo.com>
2022-05-29 15:16:36 -0500

    set up NavBar onto app

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./Navigation/Navbar.js";
+import NavBar from "./Navigation/Navbar.js";
+import Home from "./home"
+import Playlist from "./playlist"
 import SignUp from "./signup"
+import { Routes, Route } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import React, { useState } from "react"
 
 function App() {
+  const [page, setPage] = useState("/")
+
   return (
-   <div>
-      <SignUp />
-      </div>
+    <Container>
+      <NavBar onChangePage={setPage} />
+      <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/playlist" element={<Playlist/>} />
+          <Route path="*" element={<h1>404 not found</h1>} />
+      </Routes>
+    </Container>
   );
 }
 
