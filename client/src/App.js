d001713bf2d661eb2e3a640f07dc5b774ebdb785
Nicholas Freeman <nick_freeman65@yahoo.com>
2022-05-30 13:09:35 -0500

    added current user state and authentication fetch to route between the login/signup and the application if already logged in

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,66 @@ import NavBar from "./Navigation/Navbar.js";
 import Home from "./home"
 import Playlist from "./playlist"
 import SignUp from "./signup"
-import { Routes, Route } from "react-router-dom"
+import Login from "./login"
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import { Container } from "react-bootstrap"
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
 function App() {
-  const [page, setPage] = useState("/")
+  const [currentUser, setCurrentUser] = useState("")
 
+  useEffect(() => {
+    fetch('/auth')
+    .then(res => {
+      if(res.ok) {
+        res.json().then(user => setCurrentUser(user))
+      }
+    })
+  },[])
+
+  function onLogin(e) {
+    e.preventDefault()
+    console.log("works")
+  }
+  if(!currentUser) return (
+    <div className="App">
+          <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+            <div className="container">
+              <Link className="navbar-brand" to={'/sign-in'}>
+                Pods
+              </Link>
+              <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                    <Link className="nav-link" to={'/sign-in'}>
+                      Login
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to={'/sign-up'}>
+                      Sign up
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+          <div className="auth-wrapper">
+            <div className="auth-inner">
+              <Routes>
+                <Route exact path="/" element={<Login setCurrentUser={setCurrentUser} />} />
+                <Route path="/sign-in" element={<Login />} />
+                <Route path="/sign-up" element={<SignUp />} />
+              </Routes>
+            </div>
+          </div>
+        </div>
+  )
   return (
     <Container>
-      <NavBar onChangePage={setPage} />
-      <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/playlist" element={<Playlist/>} />
-          <Route path="*" element={<h1>404 not found</h1>} />
-      </Routes>
+        <NavBar />
+        <Home />
+        <Playlist />
     </Container>
   );
 }
