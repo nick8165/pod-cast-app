2afbe551df473bbbef1d79096c9612086f5a3886
Nicholas Freeman <nick_freeman65@yahoo.com>
2022-05-31 16:27:36 -0500

    fixed error from authorize to authorizeD

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,8 @@ class ApplicationController < ActionController::API
   include ActionController::Cookies
   before_action :authorized
   
-  def authorize
-    return render json:{error: "Not Authorized"}, status: :unauthorized
-    unless session.include? :user_id
+  def authorized
+    return render json:{error: "Not Authorized"}, status: :unauthorized unless session.include? :user_id
   end
 
 end
