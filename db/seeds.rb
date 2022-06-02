27f790797d8300d2ee38218b807d63b7cd5a4f6c
Nicholas Freeman <nick_freeman65@yahoo.com>
2022-06-02 16:40:36 -0500

    .

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,6 @@
-genre = Genre.create(category:"comedy")
+
+Genre.create(category: "Entertainment")
+Genre.create(category: "Role-playing Games")
+Genre.create(category: "Pop Culture")
+Genre.create(category: "Culture")
+Genre.create(category: "Technology")
