cad7e4470c9317f32fed3b4127cfd77ff0693bc4
Nicholas Freeman <nick_freeman65@yahoo.com>
2022-06-07 16:54:39 -0500

    display podcast and their episodes on click

--- client/src/browse.js
+++ client/src/browse.js
@@ -1,13 +1,36 @@
-import React from "react"
+import React, {useState} from "react"
 import {Button} from "react-bootstrap"
+import Episodes from "./episodes"
 
 function Browse({podCastList}) {
+    const [selectedPodCast, setSelectedPodCast] = useState("") 
+    
+    function handleOnClick(e) {
+        fetch(`/pod_casts/${e.target.parentNode.value}`)
+        .then(res => {
+            if(res.ok) {
+                res.json().then(ep => setSelectedPodCast(ep))
+            }
+        })
+    }
     
     function displayPodCast() {
+        if (selectedPodCast !== "") {
+            return (
+                <Episodes selectedPodCast={selectedPodCast} />
+            )
+        }
         if (podCastList !== "") {
             return podCastList.map((pod) => {
-                return (<div key={pod.title}><img src={pod.thumb_nail} alt="error" /><Button>{pod.title}</Button></div>)
+                return (<div key={pod.title}>
+                            <Button type="button" value={pod.id} className="btn btn-dark" onClick={handleOnClick}>
+                                <h4>{pod.title}</h4>
+                                <img src={pod.thumb_nail} alt="error" />
+                            </Button>
+                        </div>)
             })
+        } else {
+            return (<h1>Loading...</h1>)
         }
     }
 
